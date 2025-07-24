9c12223f835199d118a940e259545ee63e3ead9e
vinayak <vinayakshukla1239@gmail.com>
2025-07-24 18:56:52 +0530

    .

--- BASIC.R
+++ BASIC.R
@@ -4,3 +4,7 @@ z <- x + y
 print(z)
 # This code initializes two variables, x and y, with values 10 and 5 respectively,
 # then calculates their sum and stores it in variable z, which is printed to the console.
+MODEL <- ('HELLO WORLD')
+typeof(MODEL)
+# This code assigns the string 'HELLO WORLD' to the variable MODEL and checks its type,
+# which should return "character" since MODEL is a string.
