b254d20a6ebb7c4d8b4f1c4c1a2431c0a57252a9
vinayak <vinayakshukla1239@gmail.com>
2025-07-24 14:36:58 +0530

    first commit

---
+++ BASIC.R
@@ -0,0 +1,6 @@
+x <- 10
+y <- 5
+z <- x + y
+print(z)
+# This code initializes two variables, x and y, with values 10 and 5 respectively,
+# then calculates their sum and stores it in variable z, which is printed to the console.
